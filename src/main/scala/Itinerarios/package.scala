4ca21366c42598b48d65f3d4e36565f9c82dbdfb
Valentina-Nitola <valentina.nitola@correounivalle.edu.co>
2025-11-30 10:41:06 -0500

    Add itinerariosTiempo

--- src/main/scala/Itinerarios/package.scala
+++ src/main/scala/Itinerarios/package.scala
@@ -29,5 +29,46 @@ package object Itinerarios {
       buscar(cod1, cod2, Set(cod1))
   }
 
+  def itinerariosTiempo (vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
+    val its = itinerarios(vuelos, aeropuertos)
+
+    val gmtPorCod: Map[String, Int] =
+      aeropuertos.map(a => a.Cod -> a.GMT).toMap
+
+    val minutosDia = 24 * 60
+
+    def aMinutosGMT(h: Int, m: Int, codAeropuerto: String): Int = {
+      val gmt = gmtPorCod(codAeropuerto)
+      (h - gmt) * 60 + m
+    }
+
+    def ajustarForward(base: Int, valor: Int): Int =
+      if valor >= base then valor
+      else ajustarForward(base, valor + minutosDia)
+
+    def tiempoItinerario(it: Itinerario): Int = it match {
+      case Nil => 0
+      case primer :: _ =>
+        val inicio = aMinutosGMT(primer.HS, primer.MS, primer.Org)
+
+        val fin = it.foldLeft(inicio) { (tActual, vuelo) =>
+          val dep0 = aMinutosGMT(vuelo.HS, vuelo.MS, vuelo.Org)
+          val dep = ajustarForward(tActual, dep0)
+
+          val arr0 = aMinutosGMT(vuelo.HL, vuelo.ML, vuelo.Dst)
+          val arr = ajustarForward(dep, arr0)
+
+          arr
+        }
+
+        fin - inicio
+    }
+
+    (cod1: String, cod2: String) => {
+      val todos = its(cod1, cod2)
+      val ordenados = todos.sortBy(tiempoItinerario)
+      ordenados.take(3)
+    }
+  }
 
 }
--- src/test/scala/Pruebaslocal.sc
+++ src/test/scala/Pruebaslocal.sc
@@ -15,3 +15,19 @@ val its4 = itsCurso("CLO", "MEX")
 
 //2 itinerarios CTG−PTY
 val its5 = itsCurso("CTG","PTY")
+
+
+val itsTiempoCurso = itinerariosTiempo(vuelosCurso, aeropuertosCurso)
+
+// prueba itinerariosTiempo
+val itst1 = itsTiempoCurso("MID","SVCS")
+val itst2 = itsTiempoCurso("CLO","SVCS")
+
+// 4 itinerarios CLO–SVO
+val itst3 = itsTiempoCurso("CLO","SVO")
+
+// 2 itinerarios CLO–MEX
+val itst4 = itsTiempoCurso("CLO","MEX")
+
+// 2 itinerarios CTG–PTY
+val itst5 = itsTiempoCurso("CTG","PTY")
